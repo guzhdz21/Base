3e23c531c6fbecd772340620a634684b640afb1e
Arlex <labarlex17@hotmail.com>
2020-07-23 16:54:07 -0500

    Eliminar empleados

--- src/app/services/acciones.service.ts
+++ src/app/services/acciones.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { LoadingController, AlertController } from '@ionic/angular';
+import { LoadingController, AlertController, ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +7,8 @@ import { LoadingController, AlertController } from '@ionic/angular';
 export class AccionesService {
 
   constructor(private loadingCtrl: LoadingController,
-              private alertCtrl: AlertController) { }
+              private alertCtrl: AlertController,
+              private toastCtrl: ToastController) { }
 
   async presentLoading(mensaje: string, duracion: number) {
     const loading = await this.loadingCtrl.create({
@@ -18,7 +19,49 @@ export class AccionesService {
     await loading.onDidDismiss();
   }
 
-  async presentAlertGenerica( header: string, message: string) {
+  async presentToast( message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000,
+      mode: "ios",
+      color: "secondary",
+    });
+    toast.present();
+  }
+
+  async presentAlertPersonalizada( botones: any[], header: string, message: string) {
+      
+    const alert = await this.alertCtrl.create({
+      header: header,
+      message: message,
+      buttons: [] = botones,
+      mode: "md",
+      backdropDismiss: false
+    });
+    alert.present();
+    await alert.onDidDismiss();
+  }
+
+  async presentAlertConfirmacionContraseña(header: string, message: string, botones: any[]) {
+      
+    const alert = await this.alertCtrl.create({
+      header: header,
+      message: message,
+      inputs: [ {
+          name: 'contraseñaConfirmacion',
+          type: 'password',
+          value: null
+        }
+      ],
+      buttons: [] = botones,
+      mode: "md",
+      backdropDismiss: false
+    });
+    alert.present();
+    await alert.onDidDismiss();
+  }
+
+  async presentAlertGenerica(header: string, message: string) {
       
     const alert = await this.alertCtrl.create({
       header: header,
